060607a85151881d395ec195c1c3223382ca35e2
antarpreet11 <singhantarpreet11@gmail.com>
2022-10-03 04:17:36 -0400

    added user input functionality

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const https = require('https')
 const dotenv = require('dotenv')
+const bodyParser = require('body-parser')
 
 dotenv.config() 
 
 const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
 
-let city = 'waterloo,ca'
+let city = 'waterloo,canada'
 
 app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/', (req, res) => {
+    city = `${req.body.city},${req.body.country}`   
 
     const url = `${process.env.URL}q=${city}&appid=${process.env.KEY}&units=metric`
     https.get(url, (response) => {
@@ -21,13 +28,14 @@ app.get('/', (req, res) => {
             const imgUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`
             console.log(wdata)
 
-            res.write(`<h1>Temp in ${city} is ${temp} C</h1>`)
+            res.write(`<h1>Temp in ${req.body.city.toUpperCase()} is ${temp} C</h1>`)
             res.write(`<h3>The weather is currently - ${desc}</h3>`)
             res.write(`<img src=${imgUrl}>`)
             res.send()
         })
     })
 })
+
 
 app.listen(3000, () => {
     console.log('listening on port 3000')
